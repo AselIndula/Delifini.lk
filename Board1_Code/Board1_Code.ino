074653b3a4ba69e7660ccc7d37d82d91e107e966
AselIndula <35985674+AselIndula@users.noreply.github.com>
2018-05-21 23:42:14 +0530

    Performance Edition

--- Board1_Code/Board1_Code.ino
+++ Board1_Code/Board1_Code.ino
@@ -4,6 +4,8 @@ long distanceVarX1 = 0;
 long distanceVarX2 = 0;
 long distanceVarX3 = 0;
 long minX = 0;
+int dis_CountKeep[2];
+int timesIteration = 10;
 
 void setup() {
   Serial.begin  (115200);
@@ -15,8 +17,10 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 22; i < ((numberFPlots * 6) + 22 ) ; i += 6) {
-    slotX(i , i + 1 , i + 2 , i + 3 , i + 4 , i + 5);
+
+  for (int i = 22 , j = 0; i < ((numberFPlots * 6) + 22 ) ; i += 7) {
+    slotX(i , i + 1 , i + 2 , i + 3 , i + 4 , i + 5 , j);
+    j++;
   }
 
 }
@@ -33,15 +37,17 @@ long readingSensors(int trigPinx, int echoPinx) {
   return distance;
 }
 
-void slotX(int trigPinX, int echoPinX, int trigPinX1, int echoPinX1, int trigPinX2, int echoPinX2 ) {
+void slotX(int trigPinX, int echoPinX, int trigPinX1, int echoPinX1, int trigPinX2, int echoPinX2 , int slotNumber) {
+
   distanceVarX1 = readingSensors(trigPinX, echoPinX);
   distanceVarX2 = readingSensors(trigPinX1, echoPinX1);
   distanceVarX3 = readingSensors(trigPinX2, echoPinX2);
-  minMeth(distanceVarX1, distanceVarX2, distanceVarX3, minX);
-  delay(10);
+  minMeth(distanceVarX1, distanceVarX2, distanceVarX3, slotNumber);
 }
 
-void  minMeth(long distanceVarx, long distanceVary, long distanceVarz, long min) {
+void  minMeth(long distanceVarx, long distanceVary, long distanceVarz, int slotNum) {
+
+  int min = 0;
 
   if (distanceVarx < distanceVary) {
     if (distanceVarx < distanceVarz) {
@@ -56,14 +62,22 @@ void  minMeth(long distanceVarx, long distanceVary, long distanceVarz, long min)
 
   //  lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed
   if (min < 30) {
-    //    lcd.println("Stop the Car");
-    Serial.println("Stop the Car");
+
+    if (dis_CountKeep[slotNum] <= timesIteration) {
+      //    lcd.println("Stop the Car");
+      Serial.println("Stop the Car");
+      dis_CountKeep[slotNum]++;
+    }
+
   } else {
+
     //    lcd.print(min);
     //    lcd.println(" cm");
 
     Serial.print(min);
     Serial.println(" cm");
+
+    dis_CountKeep[slotNum] = 0;
   }
 }
 
